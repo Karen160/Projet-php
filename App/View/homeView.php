0012f1fc901b340edb9452f3961861b9d7a7e6cd
Alexandre Caramel <caramel.alexandre@gmail.com>
2020-11-15 14:55:39 +0100

    mise en place de la nav

--- App/View/homeView.php
+++ App/View/homeView.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="..\..\Public\Asset\img\logo.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
     <link rel="stylesheet" href="..\..\Public\Asset\css\home.css">
     <title>Accueil - Sondage</title>
 </head>
 <body>
+
 <?php include '../../inc/header.inc.php'; ?>
-    <h1>oui</h1>
+
 </body>
 </html>
--- inc/header.inc.php
+++ inc/header.inc.php
@@ -1,6 +1,8 @@
 <header>
         <nav>
             <img  src="..\..\Public\Asset\img\logo.png">
+            <i class="fas fa-user" id="user"></i>
+            <i class="fas fa-bars" id="menu"></i>
         </nav>
         
 </header>
