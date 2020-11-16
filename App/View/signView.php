1264f9d34c7879ee09bd8daf70d7f81d6be3c0f2
= <=>
2020-11-16 17:51:02 +0100

    sign and css

--- App/View/signView.php
+++ App/View/signView.php
@@ -5,7 +5,10 @@ include '../../inc/head.inc.php';
     <?php  include '../../inc/header.inc.php';?>
     <main>
             <div class="row">
-                <section class="col-sm-5 ml-5 mt-5" >
+                <div class="col-sm-1">
+
+                </div>
+                <section class="col-sm-5 mt-5" >
                     <div class="card" >
                         <div class="card-body">
                             <h2 class="card-title">Inscription</h2>
@@ -37,34 +40,22 @@ include '../../inc/head.inc.php';
                         </div>
                     </div>
                 </section>
-                <section class="col-sm-5 ml-5 mt-5" >
-                    <div class="card" >
-                        <div  class="card-body">
-                            <h2 class="card-title">Inscription</h2>
+                <section class="col-sm-5 mx-auto mt-auto mb-5 pb-5  " >
+                    <div class="card " >
+                        <div  class="card-body ">
+                            <h2 class="card-title ">Connexion</h2>
                             <div class="row">
-                                    <div class="col-sm-6">
-                                        <label for="form_name ">Nom</label>
-                                        <input id="form_name" type="text" name="nom" class="form-control" placeholder="Entrez votre nom" required="required" data-error="Le nom est requis.">
-                                    </div>
-                                    <div class="col-sm-6"> 
-                                        <label for="form_name ">Prénom</label>
-                                        <input id="form_name" type="text" name="prenom" class="form-control" placeholder="Entrez votre prénom" required="required" data-error="Le prénom est requis.">
-                                    </div>
-                                    <div class="col-sm-12 mt-4">
-                                        <label for="form_name ">Pseudo</label>
-                                        <input id="form_name" type="text" name="pseudo" class="form-control" placeholder="Choisissez un pseudo" required="required" data-error="Le pseudo est requis.">
-                                    </div>
-                                    <div class="col-sm-12 mt-4"> 
-                                        <label for="form_name ">Mail</label>
-                                        <input id="form_name" type="text" name="Mail" class="form-control" placeholder="Entrez votre email" required="required" data-error="Le mail est requis.">
-                                    </div>
-                                    <div class="col-sm-12 mt-4"> 
-                                        <label for="form_name ">Password</label>
-                                        <input id="form_name" type="password" name="Password" class="form-control" placeholder="Entrez votre mot de passe" required="required" data-error="Le mot de passe est requis.">
-                                    </div>
-                                    <div class="col-sm-12 mt-4 offset-ms-4"> 
-                                        <button class="btn btn-info btn-block active" >Envoyez</button>
-                                    </div>
+                                <div class="col-sm-12">
+                                    <label for="form_name ">Pseudo</label>
+                                    <input id="form_name" type="text" name="nom" class="form-control" placeholder="Entrez votre nom" required="required" data-error="Le nom est requis.">
+                                </div>
+                                <div class="col-sm-12 mt-4"> 
+                                    <label for="form_name ">Password</label>
+                                    <input id="form_name" type="password" name="Password" class="form-control" placeholder="Entrez votre mot de passe" required="required" data-error="Le mot de passe est requis.">
+                                </div>
+                                <div class="col-sm-12 mt-4 offset-ms-4"> 
+                                    <button class="btn btn-info btn-block active" >Envoyez</button>
+                                </div>
                             </div>
                         </div>
                     </div>
