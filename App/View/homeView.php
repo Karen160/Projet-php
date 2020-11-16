bceb53a1b583e819e96d78ba16c45d15e3c8bc57
Karen <karen.azoulay@edu.devinci.fr>
2020-11-16 15:11:25 +0100

    changement inc

--- App/View/homeView.php
+++ App/View/homeView.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="..\..\Public\Asset\img\logo.png">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
-    <link rel="stylesheet" href="..\..\Public\Asset\css\home.css">
-    <title>Accueil - Sondage</title>
-</head>
-<body>
+<?php 
+include '../../inc/head.inc.php'; 
+include '../../inc/header.inc.php'; ?>
 
-<?php include '../../inc/header.inc.php'; ?>
+
 <?php include '../../inc/footer.inc.php'; ?>
 
-</body>
-<?php include '../../inc/linkJS.inc.php'; ?>
-</html>
