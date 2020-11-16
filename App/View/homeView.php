dd68bde830b2c75a0ba4cb2a33a30e7e5bea7451
Karen <karen.azoulay@edu.devinci.fr>
2020-11-16 17:01:31 +0100

    accueil

--- App/View/homeView.php
+++ App/View/homeView.php
@@ -2,7 +2,88 @@
 include '../../inc/head.inc.php'; 
 include '../../inc/header.inc.php'; ?>
 <main>
-    
+    <h2>Nouveaux sondages</h2>
+            <div class="conteneur">
+                <div class="boxsondage">
+                    <a href="">
+                        <img src="https://static01.nyt.com/images/2016/06/24/t-magazine/24tmag-Humeau-01/24tmag-Humeau-01-articleLarge.jpg?quality=75&auto=webp&disable=upscale" alt="Demon Tanjiro">
+                        <span>48 heures</span>
+                        <p>Est ce que Tanjiro Kamado va devenir un démon ?</p>
+                    </a>
+                    <a href=""><i class="fas fa-share"></i></a>
+                </div>
+                <div class="boxsondage">
+                    <a href="demonTanjiro.php">
+                        <img src="img/onePiece/equipage.png" alt="Equipage de luffy">
+                        <span>12 points</span>
+                            <br><br>
+                            Je vais manger un hamburger
+                            <br>
+                            <i class="fas fa-arrow-right"></i>
+                        </p>
+                    </a>
+                </div>
+                <div class="boxsondage">
+                    <a href="demonTanjiro.php">
+                        <img src="img/MHA/uraraka.png" alt="Uraraka">
+                        <p>
+                            <span>11 points</span>
+                            <br><br>
+                            Ochaco va t'elle avouer ses sentiments à Izuku ?
+                            <br>
+                            <i class="fas fa-arrow-right"></i>
+                        </p>
+                    </a>
+                </div>
+                
+                <div class="boxsondage">
+                    <a href="demonTanjiro.php">
+                        <img src="img/MHA/todomido.png" alt="Combat entre todoroki et midoriya">
+                        <p>
+                            <span>24 points</span>
+                            <br><br>
+                            Quel grand combat est à prévoir avec Izuku ? 
+                            <br>
+                            <i class="fas fa-arrow-right"></i>
+                        </p>
+                    </a>
+                </div>
+                <div class="boxsondage">
+                    <a href="demonTanjiro.php">
+                        <img src="img/HxH/briguad.png" alt="Brigade fantome">
+                        <p>
+                            <span>15 points</span>
+                            <br><br>
+                            Gon et Killua vont-ils se battre contre la brigade fantôme ? 
+                            <br>
+                            <i class="fas fa-arrow-right"></i>
+                        </p>
+                    </a>
+                </div>
+                <div class="boxsondage">
+                    <a href="demonTanjiro.php">
+                        <img src="img/demonSlayer/couple.png" alt="Zenitsu et Nezuko">
+                        <p>
+                            <span>10 points</span>
+                            <br><br>
+                            Est ce que Zenitsu et Nezuko vont sortir ensemble ? 
+                            <br>
+                            <i class="fas fa-arrow-right"></i>
+                        </p>
+                    </a>
+                </div>
+            </div>
 
+            <h2>Mes sondages en cours</h2>
+            <div class="conteneur">
+                <div class="boxsondage">   
+            </div>
 
+            <h2>Derniers sondages terminés</h2>
+            <div class="conteneur">
+                <div class="boxsondage">
+            </div>
+        </section>
+
+</main>
 <?php include '../../inc/footer.inc.php'; ?>
--- inc/header.inc.php
+++ inc/header.inc.php
@@ -1,5 +1,8 @@
 <header>
     <nav>
+        <div style="width:100px">
+
+        </div>
         <img  src="..\..\Public\Asset\img\logo.png">
         <div>
             <a href="profil.php"><i class="fas fa-user"></i></a>
