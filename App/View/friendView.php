5c7da95a45630b60f29d5a9488b14e295c1591f6
Karen <karen.azoulay@edu.devinci.fr>
2020-11-17 08:13:17 +0100

    friend

---
+++ App/View/friendView.php
@@ -0,0 +1,15 @@
+<?php 
+include '../../inc/head.inc.php'; 
+include '../../inc/header.inc.php'; ?>
+<main>
+<input type="text" placeholder="Rechercher">
+<button type="submit"><i class="fa fa-search"></i></button>
+<h2>Mes amis</h2>
+<table>
+<tr>
+    <th></th>
+</tr>
+</table>
+
+</main>
+<?php include '../../inc/footer.inc.php' ?>
