df9dfceae040b81c87cc4befd361858ac80038f1
Karen <karen.azoulay@edu.devinci.fr>
2020-11-18 21:46:59 +0100

    sondage fini

--- App/View/sondage.php
+++ App/View/sondage.php
@@ -2,6 +2,15 @@
 include '../../inc/head.inc.php'; 
 include '../../inc/header.inc.php'; ?>
 <main>
+<section id="sondage">
+        <h2>La question que tout le monde attend</h2>
+        <br><br>
+        <div class="sond">
+            <button><h4>Oui</h4></button>
+            <br><br>
+            <button><h4>Non</h4></button>
+        </div>
+    </section>
     <section id="sondage">
         <h2>La question que tout le monde attend</h2>
         <br><br>
@@ -21,14 +30,23 @@ include '../../inc/header.inc.php'; ?>
             <p>30 votes</p>
         </div>
     </section>
-    <br><br>
+    <br><br><br>
     <section id="com">
         <h2>Commentaire</h2>
+        <br><br>
         <div class="msg">
             <div>
                 <img src="https://www.tbstat.com/wp/uploads/2019/07/20190724_Blockchain-Gaming.jpg">
+                <p>Pseudo</p>
+                <p>27/10</p>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident voluptates quidem ab accusamus modi cupiditate? Voluptas error ipsam repudiandae delectus amet consectetur impedit tempora neque libero? Aliquid magnam dignissimos nemo.</p>
             </div>
         </div>
+        <br>
+        <button class="btn btn-info active" style="margin:0 auto; display:block">Ajouter un commentaire</button>
+        <br>
     </section>
 
 </main>
