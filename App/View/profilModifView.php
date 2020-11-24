5f7606cef25007dddd6bd80eaabaae53a3793a52
Alexandre Caramel <caramel.alexandre@gmail.com>
2020-11-24 19:06:40 +0100

    Alex

--- App/View/profilModifView.php
+++ App/View/profilModifView.php
@@ -5,38 +5,38 @@ include '../inc/header.inc.php';
 <main id="profil">
 
     <?php var_dump($_SESSION['membre']['id']) ?>
-    
+
     <section>
         <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-        <img src="https://www.tbstat.com/wp/uploads/2019/07/20190724_Blockchain-Gaming.jpg">
-        <div class="info">
-            <div>
-                <label for="nom">Nom :</label>
-                <input type="text" name="nom" class="form-control" value="<?= $_SESSION['user']['nom'] ?>">
+            <img src="https://www.tbstat.com/wp/uploads/2019/07/20190724_Blockchain-Gaming.jpg">
+            <div class="info">
+                <div>
+                    <label for="nom">Nom :</label>
+                    <input type="text" name="nom" class="form-control" value="<?= $_SESSION['user']['nom'] ?>">
 
-                <label for="prenom">Prénom :</label>
-                <input type="text" name="prenom" class="form-control" value="<?= $afficher['prenom'] ?>">
+                    <label for="prenom">Prénom :</label>
+                    <input type="text" name="prenom" class="form-control" value="<?= $afficher['prenom'] ?>">
 
-                <label for="pseudo">Pseudo :</label>
-                <input type="text" name="pseudo" class="form-control" value="<?= $afficher['pseudo'] ?>">
+                    <label for="pseudo">Pseudo :</label>
+                    <input type="text" name="pseudo" class="form-control" value="<?= $afficher['pseudo'] ?>">
 
-                <label for="mdp">Mot de passe :</label>
-                <input type="text" name="mdp" class="form-control" required>
+                    <label for="mdp">Mot de passe :</label>
+                    <input type="text" name="mdp" class="form-control" required>
+                </div>
+                <div>
+                    <p>Nombre d'amis : <?= $afficher['nom'] ?></p>
+                    <p>Nombre de mes sondages : <?= $afficher['nom'] ?></p>
+                    <label for="email">Email :</label>
+                    <input type="text" name="email" class="form-control" value="<?= $afficher['email'] ?>">
+                    <p>Date d'inscription : <?= $afficher['date'] ?></p>
+                </div>
+
             </div>
-            <div>
-                <p>Nombre d'amis : <?= $afficher['nom'] ?></p>
-                <p>Nombre de mes sondages : <?= $afficher['nom'] ?></p>
-                <label for="email">Email :</label>
-                <input type="text" name="email" class="form-control" value="<?= $afficher['email'] ?>">
-                <p>Date d'inscription : <?= $afficher['date'] ?></p>
-            </div>
-
-        </div>
-        <button type="submit" class="btn btn-info btn-block active" name="bouton">Envoyez</button>
-        <?php
+            <button type="submit" class="btn btn-info btn-block active" name="bouton">Envoyez</button>
+            <?php
           var_dump($test);
-        ?> 
-        </form> 
+        ?>
+        </form>
 
     </section>
 </main>
--- App/View/signView.php
+++ App/View/signView.php
@@ -7,65 +7,75 @@ include '../inc/header.inc.php';
     <div class="row">
         <div class="col-sm-1">
         </div>
-        <section class="col-sm-5 position-static" id="inscription" >
-            <div class="card " >
+        <section class="col-sm-5 position-static" id="inscription">
+            <div class="card ">
                 <div class="card-body">
                     <h2 class="card-title">Inscription</h2>
-                    <form class="row " method="post"> 
-                            <div class="col-sm-6">
-                                <label for="nom">Nom</label>
-                                <input type="text" name="nom" class="form-control" placeholder="Entrez votre nom" required="required" data-error="Le nom est requis.">
-                            </div>
-                            <div class="col-sm-6 position-static"> 
-                                <label for="prenom ">Prénom</label>
-                                <input type="text" name="prenom" class="form-control" placeholder="Entrez votre prénom" required="required" data-error="Le prénom est requis.">
-                            </div>
-                            <div class="col-sm-12 mt-4 position-static">
-                                <label for="pseudo">Pseudo</label>
-                                <input type="text" name="pseudo" class="form-control" placeholder="Choisissez un pseudo" required="required" data-error="Le pseudo est requis.">
-                            </div>
-                            <div class="col-sm-12 mt-4 position-static"> 
-                                <label for="email">Email</label>
-                                <input type="email" name="email" class="form-control" placeholder="Entrez votre email" required="required" data-error="Le mail est requis.">
-                            </div>
-                            <div class="col-sm-12 mt-4 position-static"> 
-                                <label for="mdp">Password</label>
-                                <input type="password" name="mdp" class="form-control" placeholder="Entrez votre mot de passe" required="required" data-error="Le mot de passe est requis.">
-                            </div>
-                            <div class="col-sm-12 mt-4 position-static offset-ms-4"> 
-                                <button type="submit" class="btn btn-info btn-block active" name="bouton">Envoyez</button>
-                            </div>
-                            <?php
+                    <form class="row " method="post">
+                        <div class="col-sm-6">
+                            <label for="nom">Nom</label>
+                            <input type="text" name="nom" class="form-control" placeholder="Entrez votre nom"
+                                required="required" data-error="Le nom est requis.">
+                        </div>
+                        <div class="col-sm-6 position-static">
+                            <label for="prenom ">Prénom</label>
+                            <input type="text" name="prenom" class="form-control" placeholder="Entrez votre prénom"
+                                required="required" data-error="Le prénom est requis.">
+                        </div>
+                        <div class="col-sm-12 mt-4 position-static">
+                            <label for="pseudo">Pseudo</label>
+                            <input type="text" name="pseudo" class="form-control" placeholder="Choisissez un pseudo"
+                                required="required" data-error="Le pseudo est requis.">
+                        </div>
+                        <div class="col-sm-12 mt-4 position-static">
+                            <label for="email">Email</label>
+                            <input type="email" name="email" class="form-control" placeholder="Entrez votre email"
+                                required="required" data-error="Le mail est requis.">
+                        </div>
+                        <div class="col-sm-12 mt-4 position-static">
+                            <label for="mdp">Password</label>
+                            <input type="password" name="mdp" class="form-control"
+                                placeholder="Entrez votre mot de passe" required="required"
+                                data-error="Le mot de passe est requis.">
+                        </div>
+                        <div class="col-sm-12 mt-4 position-static offset-ms-4">
+                            <button type="submit" class="btn btn-info btn-block active" name="bouton">Envoyez</button>
+                        </div>
+                        <?php
+                            
                                 echo $msg;
                                 
-                            ?>  
+                            ?>
                     </form>
                 </div>
-            </form>
-            
+                </form>
+
         </section>
-        <section class="col-sm-5 mx-auto mt-5 position-static"  >
-            <div class="card " >
-                <div  class="card-body  position-static " id="connexion">
+        <section class="col-sm-5 mx-auto mt-5 position-static">
+            <div class="card ">
+                <div class="card-body  position-static " id="connexion">
                     <h2 class="card-title ">Connexion</h2>
                     <form class="row" method="post">
                         <div class="col-sm-12">
                             <label for="pseudoCo">Pseudo</label>
-                            <input type="text" name="pseudoCo" class="form-control" placeholder="Entrez votre pseudo" required="required" data-error="Le Pseudo est requis.">
+                            <input type="text" name="pseudoCo" class="form-control" placeholder="Entrez votre pseudo"
+                                required="required" data-error="Le Pseudo est requis.">
                         </div>
-                        <div class="col-sm-12 mt-4"> 
+                        <div class="col-sm-12 mt-4">
                             <label for="mdpCo">Password</label>
-                            <input type="password" name="mdpCo" class="form-control" placeholder="Entrez votre mot de passe" required="required" data-error="Le mot de passe est requis.">
+                            <input type="password" name="mdpCo" class="form-control"
+                                placeholder="Entrez votre mot de passe" required="required"
+                                data-error="Le mot de passe est requis.">
                         </div>
-                        <div class="col-sm-12 mt-4 offset-ms-4"> 
-                            <button type="submit" class="btn btn-info btn-block active" >Envoyez</button>
+                        <div class="col-sm-12 mt-4 offset-ms-4">
+                            <button type="submit" class="btn btn-info btn-block active">Envoyez</button>
                             <?php
                              echo $msgCo;
                             ?>
                         </div>
                     </form>
                 </div>
-            </form>
+                </form>
         </section>
     </div>
 </main>
