821401b1c7c8c75c6aa9554bdd168d8d7c9467a0
Karen <karen.azoulay@edu.devinci.fr>
2020-11-28 12:53:35 +0100

    pb sondage

--- App/Model/newSondageModel.php
+++ App/Model/newSondageModel.php
@@ -6,6 +6,7 @@ class newSondageModel extends Database{
   function newsondage(){  
       
     $msg = "";
+    
     // Enregistrement des éléments du sondages
     if(isset($_POST['question']) && isset($_POST['image']) && isset($_POST['proposition1'])  && isset($_POST['date'])) {
         $question = trim($_POST['question']);
@@ -16,10 +17,10 @@ class newSondageModel extends Database{
         $membre_id = $_SESSION['user']['id'];
 
         // Enregistrement de la question dans la bdd
-        $enregistrementQuestion = $pdo->prepare("INSERT INTO question (question_id, user_id_author, question, date_fin) VALUES (NULL, $membre_id, :question, :date)");
-        $enregistrement->bindParam(':question', $question, \PDO::PARAM_STR);
-        $enregistrement->bindParam(':date', $date, \PDO::PARAM_STR);
-        $enregistrement->execute();
+        $enregistrementQuestion = $this->pdo->prepare("INSERT INTO question (question_id, user_id_author, question, date_fin) VALUES (NULL, $membre_id, :question, :date)");
+        $enregistrementQuestion->bindParam(':question', $question, \PDO::PARAM_STR);
+        $enregistrementQuestion->bindParam(':date', $date, \PDO::PARAM_STR);
+        $enregistrementQuestion->execute();
 
         // Défini id de la question
         $id_question = $_SESSION['question']['question_id'];
@@ -28,9 +29,9 @@ class newSondageModel extends Database{
         for($k = 0; nbReponse>$k; $k++){
           $proposition = trim($_POST['proposition'+ ($k+1) +'']);
           // Enregistrement des proposition de réponse dans la bdd
-          $enregistrementAnswer = $pdo->prepare("INSERT INTO answer (answer_id, id_question_id, choix) VALUES (NULL, $id_question, :proposition)");
-          $enregistrement->bindParam(':proposition'+ ($k+1) +'', $proposition, \PDO::PARAM_STR);
-          $enregistrement->execute();
+          $enregistrementAnswer = $this->pdo->prepare("INSERT INTO answer (answer_id, id_question_id, choix) VALUES (NULL, $id_question, :proposition)");
+          $enregistrementAnswer->bindParam(':proposition'+ ($k+1) +'', $proposition, \PDO::PARAM_STR);
+          $enregistrementAnswer->execute();
         }
         
         $msg = "<div class='alertGlobal2'>Merci ! Votre sondage a bien été enregistré !</div>";
