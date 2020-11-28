2e33856aca024ece2ef896124d83d64a3af75861
= <=>
2020-11-28 20:20:15 +0100

    mdp verif

--- App/Model/ProfilmodifModel.php
+++ App/Model/ProfilmodifModel.php
@@ -1,44 +1,57 @@
-<?php
-namespace App\Model;
+<?php namespace App\Model;
 use Core\Database;
 
-class ProfilModifModel extends Database{
-function recup(){
-    return $this->query("SELECT * from user where id = ".$_SESSION['user']['id']);
-}
-function modifier(){
+class ProfilModifModel extends Database {
+    function recup() {
+        return $this->query("SELECT * from user where id = ".$_SESSION['user']['id']);
+    }
+
+    function modifier() {
         $id = $_SESSION['user']['id']; //récup id 
-        $msg = "";
+        if(isset($_POST['bouton'])) {
+            if(isset($_POST['prenom']) && isset($_POST['nom']) && isset($_POST['pseudo']) && isset($_POST['email']) && isset($_POST['mdp'])) {
+                $mdp=trim($_POST['mdp']);
+                var_dump($mdp);
+                $recup_infos=$this->pdo->query("SELECT * FROM user WHERE id = '$id' ");
+                var_dump($recup_infos);
+                $infos_membre=$recup_infos->fetch(\PDO::FETCH_ASSOC);
+                var_dump($infos_membre);
 
-    if(isset($_POST['bouton'])){
-        if(isset($_POST['prenom']) && isset($_POST['nom']) && isset($_POST['pseudo']) && isset($_POST['email']) && isset($_POST['mdp'])) {
+                 if(password_verify($mdp, $infos_membre['mdp']) == true ) {
+                    if(isset($_POST['Nmdp'])){
+                        $Nmdp=trim($_POST['Nmdp']);
+                        $Nmdp=password_hash($Nmdp, PASSWORD_DEFAULT);
+                    }else{
+                        $Nmdp = $mdp;
+                        $Nmdp=password_hash($Nmdp, PASSWORD_DEFAULT);
+                    }
+                    $prenom=trim($_POST['prenom']);
+                    $nom=trim($_POST['nom']);
+                    $pseudo=trim($_POST['pseudo']);
+                    $email=trim($_POST['email']);
 
-        $mdp = trim($_POST['mdp']);
-        $mdp = password_hash($mdp, PASSWORD_DEFAULT);
-        $prenom = trim($_POST['prenom']);
-        $nom = trim($_POST['nom']);
-        $pseudo = trim($_POST['pseudo']);
-        $email = trim($_POST['email']);
 
+                    $enregistrement=$this->pdo->prepare("UPDATE user SET nom = :nom , prenom = :prenom, pseudo = :pseudo, email = :email, mdp = :Nmdp WHERE id = '$id' ");
 
-        $enregistrement = $this->pdo->prepare("UPDATE user SET nom = :nom , prenom = :prenom, pseudo = :pseudo, email = :email, mdp = :mdp WHERE id = $id");
-        
-        $enregistrement->bindParam(':nom', $nom, \PDO::PARAM_STR);
-        $enregistrement->bindParam(':prenom', $prenom, \PDO::PARAM_STR);
-        $enregistrement->bindParam(':pseudo', $pseudo, \PDO::PARAM_STR);
-        $enregistrement->bindParam(':email', $email, \PDO::PARAM_STR);
-        $enregistrement->bindParam(':mdp', $mdp, \PDO::PARAM_STR);
-        $enregistrement->execute();
+                    $enregistrement->bindParam(':nom', $nom, \PDO::PARAM_STR);
+                    $enregistrement->bindParam(':prenom', $prenom, \PDO::PARAM_STR);
+                    $enregistrement->bindParam(':pseudo', $pseudo, \PDO::PARAM_STR);
+                    $enregistrement->bindParam(':email', $email, \PDO::PARAM_STR);
+                    $enregistrement->bindParam(':Nmdp', $Nmdp, \PDO::PARAM_STR);
+                    $enregistrement->execute();
 
-        //rediriger
-        header("location:index.php?page=profil");
-     } else {
-        return $msg = "<div style='margin: 10px auto; padding:10px 0; width: 90%; background-color: red; text-transform: uppercase; color: white; text-align: center;'>Veuiller remplir tous les champs</div>";
-        
-    }
- 
-    }
+                    //rediriger
+                    header("location:index.php?page=profil");
+                 } else {
+                //     return $mes = true;
+                //     return $msg="<div style='margin: 10px auto; padding:10px 0; width: 90%; background-color: red; text-transform: uppercase; color: white; text-align: center;'>Mdp inccorrect</div>";
+                }
+
+            }else{
+                return $mes = true;
+                return $msg="<div style='margin: 10px auto; padding:10px 0; width: 90%; background-color: red; text-transform: uppercase; color: white; text-align: center;'>Veuiller remplir tous les champs</div>";
+            }
         }
     }
-
-?>
+}
+    ?>
