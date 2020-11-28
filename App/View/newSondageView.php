b8e02749ecc543f01cb17cbbe1621ba316e27f47
Karen <karen.azoulay@edu.devinci.fr>
2020-11-28 12:29:15 +0100

    karen

--- App/View/newSondageView.php
+++ App/View/newSondageView.php
@@ -7,16 +7,20 @@ include '../../inc/header.inc.php'; ?>
             <form class="card-body" method="post">
                 <h2 class="card-title">Créer un sondage</h2>
                 <div class="  col-sm-12 mt-3">
-                    <label for="form_name ">Question</label>
-                    <input type="text" name="question" class="form-control" placeholder="Entrez votre question" required="required" data-error="La question est requise.">
+                    <label for="question">Question</label>
+                    <input type="text" name="question" class="form-control" placeholder="Entrez votre question"
+                        required="required" data-error="La question est requise.">
                 </div>
-                <div class="  col-sm-12 mt-3"> 
-                    <label for="form_name ">Lien de l'image</label>
-                    <input type="text" name="image" class="form-control" placeholder="Entrez le lien de votre image" required="required" data-error="L'image est requise.">
+                <div class="  col-sm-12 mt-3">
+                    <label for="image">Lien de l'image</label>
+                    <input type="text" name="image" class="form-control" placeholder="Entrez le lien de votre image"
+                        required="required" data-error="L'image est requise.">
                 </div>
                 <div class="  col-sm-12 mt-3">
                     <label for="reponseNb ">Nombre de réponse</label>
-                    <select id="reponseNb" type="text" name="nbquestion" class="form-control" placeholder="Nombre de vos proposition de réponse" required="required" data-error="Le nombre de réponse est requis.">
+                    <select id="reponseNb" type="text" name="nbquestion" class="form-control"
+                        placeholder="Nombre de vos proposition de réponse" required="required"
+                        data-error="Le nombre de réponse est requis.">
                         <option value="0" selected>Selectionnez un nombre</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
@@ -29,14 +33,15 @@ include '../../inc/header.inc.php'; ?>
                         <option value="10">10</option>
                     </select>
                 </div>
-                <div id="proposition">
+                <div id="proposition" required="required">
                 </div>
-                <div class="col-sm-12 mt-3"> 
-                    <label for="form_name">Date d'expiration</label>
-                    <input  type="date" name="date" class="form-control" placeholder="Choisissez la date d'expiration" required="required" data-error="La date d'expiration est requise.">
+                <div class="col-sm-12 mt-3">
+                    <label for="date">Date d'expiration</label>
+                    <input type="date" name="date" class="form-control" placeholder="Choisissez la date d'expiration"
+                        required="required" data-error="La date d'expiration est requise.">
                 </div>
-                <div class="  col-sm-12 mt-4 offset-ms-4"> 
-                    <button class="btn btn-info btn-block active" >Envoyez</button>
+                <div class="  col-sm-12 mt-4 offset-ms-4">
+                    <button id="bouton" type="submit" class="btn btn-info btn-block active">Envoyez</button>
                 </div>
             </form>
         </div>
@@ -45,16 +50,26 @@ include '../../inc/header.inc.php'; ?>
 <?php include '../../inc/footer.inc.php'?>
 
 <script>
-
-//Permet d'afficher le bon nombre de champ a remplir pour le choix de proposition de réponse que souhaite mettre l'internaute sur sa question
-    $("#reponseNb").change(function(){
+    //Permet d'afficher le bon nombre de champ a remplir pour le choix de proposition de réponse que souhaite mettre l'internaute sur sa question
+    var i = 0;
+    $("#reponseNb").change(function () {
         let nbInput = $(this).val();
-       
+
         $("#proposition").html("");
-        i=0;
-        while(i<nbInput){
-            $("#proposition").append("<div class='col-sm-12 mt-3'> <label for='proposition'>Proposition " + (i+1) + "</label><input type='text' name='proposition' class='form-control' placeholder='Entrez votre proposition' required='required'></div>");
+        
+        while (i < nbInput) {
+            $("#proposition").append("<div class='col-sm-12 mt-3'> <label for='proposition'>Proposition " + (i +
+                    1) +
+                "</label><input type='text' name='proposition" + (i+1) + "' class='form-control' placeholder='Entrez votre proposition' required='required'></div>"
+                );
             i++;
-    }
+        }
     });
+
+    $("#bouton").click(function(){
+        var nbReponse = i;
+        console.log(nbReponse);
+    })
+    
+    
 </script>
