4092a6dcd3040aeec7335f348b93c5fc7b36eb92
Karen <karen.azoulay@edu.devinci.fr>
2020-11-28 20:20:35 +0100

    karen

--- App/Model/newSondageModel.php
+++ App/Model/newSondageModel.php
@@ -7,6 +7,7 @@ class newSondageModel extends Database{
       
     $msg = "";
 
+
     // Enregistrement des éléments du sondages
     if(isset($_POST['question']) && isset($_POST['image']) && isset($_POST['proposition1'])  && isset($_POST['date'])) {
         $question = trim($_POST['question']);
@@ -14,8 +15,7 @@ class newSondageModel extends Database{
         $date = trim($_POST['date']);
 
         // Défini id_membre de l'internaute connecté = membre_id de la table article en bdd
-        $membre_id = $_SESSION['membre']['id_membre'];
-        $pseudo_membre = $_SESSION['membre']['pseudo'];
+        $membre_id = $_SESSION['user']['id'];
 
         // Enregistrement de la question dans la bdd
         $enregistrementQuestion = $this->pdo->prepare("INSERT INTO question (question_id, user_id_author, question, image, date_fin) VALUES (NULL, $membre_id, :question, :image, :date)");
@@ -25,18 +25,24 @@ class newSondageModel extends Database{
         $enregistrementQuestion->execute();
 
         // Défini id de la question
-        $id_question = $_SESSION['question']['question_id'];
+        $recup_question = $this->pdo->query("SELECT * FROM question");
+        $recupQ = $recup_question->fetch(\PDO::FETCH_ASSOC);
+        $id_question = $recupQ['question']['question_id'];
+        var_dump($id_question);
 
+        $nbRep = $_GET['nbReponse'];
+        var_dump($nbRep);
        
-        for($k = 0; nbReponse>$k; $k++){
+        for($k = 0; $nbRep>$k; $k++){
           $proposition = trim($_POST['proposition'+ ($k+1) +'']);
           // Enregistrement des proposition de réponse dans la bdd
           $enregistrementAnswer = $this->pdo->prepare("INSERT INTO answer (answer_id, id_question_id, choix) VALUES (NULL, $id_question, :proposition)");
           $enregistrementAnswer->bindParam(':proposition'+ ($k+1) +'', $proposition, \PDO::PARAM_STR);
           $enregistrementAnswer->execute();
         }
-        
-        $msg = "<div class='alertGlobal2'>Merci ! Votre sondage a bien été enregistré !</div>";
+
+        return  $msg = "<div style='margin: 10px auto; padding:10px 0; width: 90%; background-color: green; text-transform: uppercase; color: white; text-align: center;'>Merci ! Votre sondage a bien été enregistré !</div>";
+
     }
 
     $articles = $this->pdo->query("SELECT id_article, titre, pseudo, image FROM article");
