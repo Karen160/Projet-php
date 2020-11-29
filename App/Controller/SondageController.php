dc432b07b66f6130defba62ed77f25b1898268c1
= <=>
2020-11-29 23:42:57 +0100

    affichage par url plus redirection si not exist)

--- App/Controller/SondageController.php
+++ App/Controller/SondageController.php
@@ -6,17 +6,20 @@ class SondageController {
 
     public function __construct() {
         $this->model=new SondageModel();
+
     }
 
     public function render() {
         if(($_SESSION['connect'])) {
-            $sondage=$this->model->sondage();
-            require ROOT."/App/View/sondageView.php";
-        }
-
-        else {
+            $id=$this->model->verif();
+            if( !empty($id) &&  !empty($_GET['sondage'])) {
+                $sondage=$this->model->sondage();
+                require ROOT."/App/View/sondageView.php";
+            }else {
+                header('location:index.php?page=home');
+            }
+        }else{
             header('location:index.php?page=home');
-
         }
     }
 }
--- App/Model/SondageModel.php
+++ App/Model/SondageModel.php
@@ -3,9 +3,21 @@ namespace App\Model;
 use Core\Database;
 
 class SondageModel extends Database{ 
+  function verif(){
+    $sondage_id = $_GET['sondage'];
+
+    return $id = $this->query("SELECT `question_id` FROM `question` where  `question_id` =  '$sondage_id'  ");
+  }
   function sondage(){  
       $sondage_id = $_GET['sondage'];
+
+    //select tout les ids de sondage exitants
+      
+      
+      //select info d'un sondage
       return $sondage = $this->query("SELECT `question`, `question_id` FROM `question` where `question_id` = ' $sondage_id' ");
+      
+       //return de variables
       
       // $resultSondage = 
     }
