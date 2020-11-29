d0d013f07b4c34f28aecf5e218c548ba1f5747b4
= <=>
2020-11-29 18:49:44 +0100

    push

--- App/Model/NewFriendModel.php
+++ App/Model/NewFriendModel.php
@@ -5,7 +5,8 @@ use Core\Database;
 class NewFriendModel extends Database{
     function friend(){
 
-    $Result = $this->pdo->query("SELECT pseudo FROM user WHERE id <>".$_SESSION['user']['id']);
+    $Result = $this->query("SELECT pseudo FROM user WHERE id <>".$_SESSION['user']['id']);
+
     // if(!empty($_POST['recherche'])) {     
     //     $recherche = htmlspecialchars($_POST['recherche']);
     //     $Result = $this->pdo->query("SELECT pseudo FROM user WHERE pseudo LIKE "%'.$recherche.'%" ORDER BY id DESC ");
