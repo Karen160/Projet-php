44a6180018ad3c7b3a0fb0a68521cf1a1604ed88
= <=>
2020-11-29 19:42:04 +0100

    push

--- App/View/newFriendView.php
+++ App/View/newFriendView.php
@@ -5,9 +5,10 @@ include '../inc/header.inc.php';
 
 <main>
     <a href="../public/index.php?page=friend" class="btn btn-info active" style="float:right; margin-right:40px">Voir mes amis</a><br><br>
-    <form class="form-inline">
-        <input class="form-control mr-sm-0" type="search" placeholder="Rechercher" aria-label="Search" name="recherche">
-        <button class="btn btn-outline-success my-2 my-sm-0 active" type="submit">
+    <form method="POST" class="form-inline">
+        <input name="recherche" class="form-control mr-sm-0" type="search" placeholder="Rechercher" aria-label="Search" >
+        
+        <button name="button" class="btn btn-outline-success my-2 my-sm-0 active" type="submit">
             <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="white" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
                 <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
@@ -26,15 +27,20 @@ include '../inc/header.inc.php';
             </tr>
         </thead>
         <tbody>
-
+        
             <?php 
-            foreach($Result as $user): ?>
+             
+            foreach($var[0] as $user): ?>
             <tr>
                 <td> <?= $user->pseudo ?> </td>
                 <td>Connecté</td>
                 <td><button>Ajouter</button></td>
             </tr>
-            <?php endforeach ?>
+           
+            <?php 
+            
+            endforeach ?>
+            <div style="margin-left:25.5%;text-align: center; background-color : orange; width : 49%"><?php echo $var[1]; ?></div>
         </tbody>
     </table>
 </main>
--- App/View/profilView.php
+++ App/View/profilView.php
@@ -5,7 +5,7 @@ include '../inc/header.inc.php';
 ?>
 <main id="profil">
     <button onclick="window.location.href = 'index.php?page=profilModif'" class="btn btn-info active" style="float:right; margin-right:40px">Modifier mon profil</button><br><br> 
-    <button onclick="window.location.href = 'friendView.php'" class="btn btn-info active" style="float:right; margin-right:70px">Mes amis</button>
+    <button onclick="window.location.href = 'index.php?page=friend'" class="btn btn-info active" style="float:right; margin-right:70px">Mes amis</button>
    
     <section>
         <img src="https://www.tbstat.com/wp/uploads/2019/07/20190724_Blockchain-Gaming.jpg">
