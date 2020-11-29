02ecb07c100d383bc8da2fd9dfb7ee636b180b2e
= <=>
2020-11-29 15:34:00 +0100

    fix miss tap git

--- App/Model/HomeModel.php
+++ App/Model/HomeModel.php
@@ -10,7 +10,7 @@ class HomeModel extends Database{
         $membre_id = $_SESSION['user']['id'];
         $sond = $this->query(" SELECT q.`question`, u.`pseudo`, q.`image`, q.`date_fin` FROM `question` as q INNER JOIN `user` as u on q.`user_id_author` = u.`id` WHERE date_fin >= CURDATE() ORDER BY date_fin ASC");
         
-        $sondPerso = $this->query("SELECT question, image, date_fin FROM question WHERE date_fin >= CURDATE() and `user_id_author` = '$membre_idgit' ");    
+        $sondPerso = $this->query("SELECT question, image, date_fin FROM question WHERE date_fin >= CURDATE() and `user_id_author` = '$membre_id' ");    
         return $requete = array($sond, $sondPerso);
     }
 }
