22017dfceb38f8c960830e0cf9d82f1938f7fbd3
= <=>
2020-11-30 18:59:41 +0100

    add friends

--- App/Model/NewFriendModel.php
+++ App/Model/NewFriendModel.php
@@ -6,33 +6,43 @@ class NewFriendModel extends Database {
 
         $Result=$this->query("SELECT * FROM user WHERE id <>".$_SESSION['user']['id']);
         $msg="";
+        $msg2="";
 
         if(isset($_POST['button'])) {
             if( !empty($_POST['recherche'])) {
                 $recherche=htmlspecialchars(trim($_POST['recherche']));
                 $Result=$this->query("SELECT * FROM user WHERE pseudo LIKE '$recherche%' ORDER BY id DESC ");
-                $rowCount = $this->pdo->query("SELECT * FROM user WHERE pseudo LIKE '$recherche%' ORDER BY id DESC ");
-                if( $rowCount->rowCount() < 1) {
+                $rowCount=$this->pdo->query("SELECT * FROM user WHERE pseudo LIKE '$recherche%' ORDER BY id DESC ");
+
+                if($rowCount->rowCount() < 1) {
                     $msg='0 amis pour toi magueule';
                 }
             }
         }
-        if(isset($_GET['id'])) {
-        $test = $_GET['id'];
-        $idB = $_GET['id'];
-        $idA = $_SESSION['user']['id'];
 
-        $FriendAdd =$this->pdo->prepare("INSERT INTO friend (user_id_A, user_id_B) VALUES ('$idA', '$idB')");
-        $FriendAdd->execute();
-        }else{
-            $idA = $_SESSION['user']['id'];
-            $test = 8;
-            $idB = NULL;
+        if(isset($_GET['id'])) {
+            $idFriend=$_GET['id'];
+            $idUser=$_SESSION['user']['id'];
+            //verif présence amis dans col A 
+            $colA=$this->pdo->query("SELECT user_id_A FROM friend where user_id_A = '$idFriend'  AND user_id_B = '$idUser' ");
+            $colB=$this->pdo->query("SELECT user_id_B FROM friend where user_id_A = '$idUser'  AND user_id_B = '$idFriend' ");
+
+            if($colA->rowCount()==0 && $colB->rowCount()==0) {
+                $FriendAdd=$this->pdo->prepare("INSERT INTO friend (user_id_A, user_id_B) VALUES ('$idUser', '$idFriend')");
+                $FriendAdd->execute();
+            }
+
+            else {
+               $msg2 = 'vous êtes déjà amis';
+            }
+
+
         }
-        return $var = array($Result, $msg, $test, $idA, $idB);
+
+        return $var=array($Result, $msg, $msg2);
 
     }
 
 
-    
+
 }
