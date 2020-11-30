cf6662ffd0370fab7f7a4d9a79307090f52ea47e
Karen <karen.azoulay@edu.devinci.fr>
2020-11-30 21:37:04 +0100

    ka

--- App/View/sondageView.php
+++ App/View/sondageView.php
@@ -60,5 +60,48 @@ include '../inc/header.inc.php'; ?>
         <br>
     </section>
 
+    <div id="fond"></div>
+        <section class="col-sm-7 mx-auto" id="shareSondage">
+            <div class="card position-static">
+                <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+                <i class="fas fa-times"></i>
+                <div class="card-body">
+                    <h2 class="card-title">Partager le sondage<br>la question de folie</h2>
+                    <div class="row ">
+                        <div class="col-sm-12 mt-4">
+                            <label for="nbPerson">Nombre de personne</label>
+                            <select id="formNbPerson" type="text" name="nbpersonne" class="form-control"
+                                placeholder="Choisissez le nombre de personne à qui partager" required="required"
+                                data-error="Le nombre de personne est requis.">
+                                <option value="0" selected>Veuillez selectionner un nombre</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                            </select>
+                        </div>
+                        <div id="email">
+
+                        </div>
+                        <div class="col-sm-12 mt-4">
+                            <label for="message">Message</label>
+                            <input id="message" name="msg" class="form-control" value="Salut, '<?php ?>'" rows="5">
+                        </div>
+                        <div class="col-sm-12 mt-4 offset-ms-4">
+                            <button type="submit" class="btn btn-info btn-block active">Envoyez</button>
+                        </div>
+                        <?php echo $msg ?>
+                    </div>
+                </div>
+                </form> 
+            </div>
+        </section>
+
 </main>
 <?php include '../inc/footer.inc.php'?>
