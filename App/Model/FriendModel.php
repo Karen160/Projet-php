5c8afc7c8a6a9c5967d693a77d24fcd0c1647e0d
Alexandre Caramel <caramel.alexandre@gmail.com>
2020-11-30 19:26:53 +0100

    :rocket show friend

--- App/Model/FriendModel.php
+++ App/Model/FriendModel.php
@@ -5,9 +5,21 @@ use Core\Database;
 class FriendModel extends Database{
     function friend(){
 
-    // if(!empty($_POST['recherche'])) {     
-    //     $recherche = htmlspecialchars($_POST['recherche']);
-    //     $searchresult = $this->pdo->query("SELECT pseudo FROM user WHERE pseudo LIKE "%'.$recherche.'%" ORDER BY id DESC ");
-    // }
+        $idUser=$_SESSION['user']['id'];
+        $colA=$this->pdo->query("SELECT user_id_A FROM friend WHERE user_id_B = '$idUser'");
+        $colB=$this->pdo->query("SELECT user_id_B FROM friend WHERE user_id_A = '$idUser'");
+
+        if($colA->rowCount()==1 || $colB->rowCount()==1) {
+            var_dump($colA);
+            var_dump($colB);
+        }else{
+            $msg = "vous n'avez aucun amis";
+        }
+
+
+    if(!empty($_POST['recherche'])) {     
+        $recherche = htmlspecialchars($_POST['recherche']);
+        $searchresult = $this->pdo->query("SELECT pseudo FROM user WHERE pseudo LIKE "%'.$recherche.'%" ORDER BY id DESC ");
+    }
     }
 }
